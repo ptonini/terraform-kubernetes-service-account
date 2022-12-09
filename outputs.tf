ad43018d3e0a27dd38aace120ab85530d433ed91
Pedro Tonini <51178173+ptonini@users.noreply.github.com>
2022-12-09 18:36:37 -0300

    feat: added create bindings flag and rules output

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "this" {
   value = kubernetes_service_account_v1.this
 }
 
+output "cluster_roles" {
+  value = kubernetes_cluster_role.this
+}
+
 output "token" {
   value = kubernetes_secret_v1.this.data.token
 }
--- variables.tf
+++ variables.tf
@@ -9,13 +9,17 @@ variable "create_token" {
 variable "cluster_role_rules" {
   type = list(object({
     api_groups = list(string)
-    resources = list(string)
-    verbs = list(string)
+    resources  = list(string)
+    verbs      = list(string)
   }))
   default = []
 }
 
 variable "cluster_role_bindings" {
-  type = list(string)
+  type    = list(string)
   default = []
+}
+
+variable "create_bindings" {
+  default = true
 }
