ea8c58f7ce569e294e1e6b6497221a4647a25db1
ptonini <pedro.tonini@hotmail.com>
2023-12-14 12:31:26 -0300

    feat: updated outputs

--- main.tf
+++ main.tf
@@ -17,24 +17,27 @@ resource "kubernetes_secret_v1" "this" {
 }
 
 module "cluster_role" {
-  source = "ptonini/cluster-role/kubernetes"
+  source  = "ptonini/cluster-role/kubernetes"
   version = "~> 1.0.0"
-  count  = length(var.cluster_role_rules) > 0 ? 1 : 0
-  name   = kubernetes_service_account_v1.this.metadata[0].name
-  rules  = var.cluster_role_rules
+  count   = length(var.cluster_role_rules) > 0 ? 1 : 0
+  name    = kubernetes_service_account_v1.this.metadata[0].name
+  rules   = var.cluster_role_rules
 }
 
 resource "kubernetes_cluster_role_binding" "this" {
   provider = kubernetes
   for_each = var.create_bindings ? toset(compact(concat([module.cluster_role[0].this.metadata[0].name], var.cluster_role_bindings))) : []
+
   metadata {
     name = kubernetes_service_account_v1.this.metadata[0].name
   }
+
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "ClusterRole"
     name      = each.value
   }
+
   subject {
     kind      = "ServiceAccount"
     name      = kubernetes_service_account_v1.this.metadata[0].name
--- outputs.tf
+++ outputs.tf
@@ -2,10 +2,6 @@ output "this" {
   value = kubernetes_service_account_v1.this
 }
 
-output "token" {
-  value = kubernetes_secret_v1.this.data.token
-}
-
-output "ca_crt" {
-  value = kubernetes_secret_v1.this.data["ca.crt"]
+output "secret" {
+  value = kubernetes_secret_v1.this.data
 }
